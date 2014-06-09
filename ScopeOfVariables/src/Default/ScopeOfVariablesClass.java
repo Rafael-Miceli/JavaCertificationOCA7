4681610972147b9e7ec944751df7e388bdb70c44
Rafael Miceli <rafael.miceli@hotmail.com>
2014-06-09 17:10:51 -0300

    Studying variables scope

--- ScopeOfVariables/src/Default/ScopeOfVariablesClass.java
+++ ScopeOfVariables/src/Default/ScopeOfVariablesClass.java
@@ -1,5 +1,22 @@
 package Default;
 
 public class ScopeOfVariablesClass {
-
+	
+	public static void main(String args[]){
+		
+		{
+			int num1 = 10;
+			
+			System.out.println(num1);
+		}
+		
+		{
+			int num1 = 20;
+			int $num1 = 12;
+			System.out.println(num1);
+			System.out.println($num1);
+		}
+		
+	}
+	
 }
